39d008f0eca9a3457efd5e0f701c27229be3cedd
Benoist <mail@benoistbabin.fr>
2026-07-05 10:32:29 +0200

    Fix lint telemetry

--- packages/telemetry/.eslintrc.cjs
+++ packages/telemetry/.eslintrc.cjs
@@ -1,5 +1,21 @@
 module.exports = {
   root: true,
-  extends: ["../../.eslintrc.cjs"],
-  parserOptions: { project: "./tsconfig.json" },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {},
 };
